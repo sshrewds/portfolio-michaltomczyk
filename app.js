6304936a0647f2a5d24248608aea691ae8fc5f4c
Michal Tomczyk <sshrewds@gmail.com>
2021-07-01 03:36:45 +0200

    mobile and desktop

---
+++ app.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-active');
+    navigation.classList.toggle('navigation-active');
+    console.log(navigation);
+});
+
+const skillItems = document.querySelectorAll('.skill-item');
+skillItems.forEach((item)=>item.addEventListener('click', ()=>{
+    item.classList.toggle('skill-item-active');
+}));
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const section = document.querySelector(this.getAttribute('href'));
+        const y = section.getBoundingClientRect().top + window.pageYOffset -80;
+
+        window.scrollTo({top: y, behavior : 'smooth'});
+    });
+});
